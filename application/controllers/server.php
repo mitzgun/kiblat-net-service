18fa7c8c3e5034c5ba5b3a2205a9d900fcac803c
rakhmadikhsanudin <rakhmadikhsanudin@ed23b9b0-44a4-140e-26a3-ae2f05e6dc4f>
2013-11-07 01:03:36 +0000

    client, server, model

--- application/controllers/server.php
+++ application/controllers/server.php
@@ -1,29 +1,46 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 /**
-* 
-*/
+ *
+ */
 class Server extends CI_Controller {
 
-	function __construct()
-	{
+	function __construct() {
 		parent::__construct();
-		$this->load->model('post');
-	}
-	public function index()
-	{
-		$this->load->library('xmlrpc');
-		$this->load->library('xmlrpcs');
-		$config['functions']['getPost'] = array('function' => 'server.getPost');
-		$this->xmlrpcs->initialize($config);
-        $this->xmlrpcs->serve();
+		$this -> load -> model('post');
 	}
 
-	function getPost(){
-		$data = $this->post->selectPost();
-		 $response = array(json_encode($data));
-        return $this->xmlrpc->send_response($response);
-    }
+	public function index() {
+		$this -> load -> library('xmlrpc');
+		$this -> load -> library('xmlrpcs');
+		$config['functions']['getPost'] = array('function' => 'server.getPost');
+		$config['functions']['getPopularTag'] = array('function' => 'server.getPopularTag');
+		$config['functions']['getPostbyIdTag'] = array('function' => 'server.getPostbyIdTag');
+		$this -> xmlrpcs -> initialize($config);
+		$this -> xmlrpcs -> serve();
+	}
+
+	function getPost() {
+		$data = $this -> post -> selectPost();
+		$response = array(json_encode($data));
+		return $this -> xmlrpc -> send_response($response);
+	}
+
+	function getPopularTag() {
+		$data = $this -> post -> populartag();
+		$response = array(json_encode($data));
+		return $this -> xmlrpc -> send_response($response);
+	}
+	
+	function getPostbyIdTag() {
+		//$parameters = $request -> output_parameters();
+		//$id=$parameters['0'];
+		$data = $this -> post -> postbyidtag(21);
+		$response = array(json_encode($data));
+		return $this -> xmlrpc -> send_response($response);
+	}
 
 }
 
