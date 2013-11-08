fe845ef2e7af6692b77532ae6612be7470b8f9cb
rakhmadikhsanudin <rakhmadikhsanudin@ed23b9b0-44a4-140e-26a3-ae2f05e6dc4f>
2013-11-08 08:16:41 +0000

    tambah html_dom

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -7,27 +7,27 @@
 
 class client extends CI_Controller {
 
-    function __construct() {
-        parent::__construct();
-        $this->load->helper('url');
-        $this->load->library('xmlrpc');
-        $server_url = site_url('server');
-        $this->xmlrpc->server($server_url, 80);
-    }
+	function __construct() {
+		parent::__construct();
+		$this -> load -> helper('url');
+		$this -> load -> library('xmlrpc');
 
+		$server_url = site_url('server');
+		$this -> xmlrpc -> server($server_url, 80);
+	}
 
-    function index(){
-    	$this->xmlrpc->method('getPostbyIdTag');
-    	//$this->xmlrpc->set_debug(TRUE);
-    	      if (!$this->xmlrpc->send_request()) {
-            echo $this->xmlrpc->display_error();
+	function index() {
+		$this -> xmlrpc -> method('GetPost');
+		//$this->xmlrpc->set_debug(TRUE);
+		if (!$this -> xmlrpc -> send_request()) {
+			echo $this -> xmlrpc -> display_error();
 
-        } else {
-            echo '<pre>';
-            print_r($this->xmlrpc->display_response());
-            echo '</pre>';
-        }
-    }
-    }
+		} else {
+			//echo '<pre>';
+			print_r($this -> xmlrpc -> display_response());
+			//echo '</pre>';
+		}
+	}
 
-  ?>
+}
+?>
