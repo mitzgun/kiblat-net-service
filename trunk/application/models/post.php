208129cd2acd6a16270bee51cc25604491fb7d7e
rakhmadikhsanudin <rakhmadikhsanudin@ed23b9b0-44a4-140e-26a3-ae2f05e6dc4f>
2013-11-09 04:41:13 +0000

    commit semua

--- trunk/application/models/post.php
+++ trunk/application/models/post.php
@@ -37,14 +37,15 @@ class post extends CI_Model {
 		 ORDER BY
 		 p1.post_date DESC LIMIT 1";
 		 */
-		$query = "select wp_posts.ID, wp_posts.post_date, wp_posts.post_content, wp_posts.post_title, wp_posts.guid, wp_terms.name
+		$query = "select wp_posts.ID, wp_posts.post_date, wp_posts.post_content, wp_posts.post_title, wp_posts.guid, wp_terms.name, CONVERT(wp_postmeta.meta_value,UNSIGNED INTEGER) as count
 		from wp_posts
 		join wp_term_relationships on wp_posts.ID = wp_term_relationships.object_id
 		join wp_term_taxonomy on wp_term_relationships.term_taxonomy_id= wp_term_taxonomy.term_taxonomy_id
 		join wp_terms on wp_term_taxonomy.term_id=wp_terms.term_id
-		where wp_posts.post_status='publish'
-		GROUP BY wp_posts.id
-		order by wp_posts.post_date desc
+		join wp_postmeta on wp_posts.ID=wp_postmeta.post_id
+		where wp_posts.post_status='publish' and wp_postmeta.meta_key='wpb_post_views_count' and wp_posts.post_type='post' and WEEKOFYEAR(NOW())=WEEKOFYEAR(wp_posts.post_date)
+		GROUP BY wp_posts.ID
+		order by  wp_posts.post_date desc
 		limit 10";
 		$post = $this -> db -> query($query);
 		return $post -> result_array();
@@ -79,6 +80,42 @@ class post extends CI_Model {
 		return $post -> result_array();
 
 	}
+	
+	function popularpost() {
+		$query = "
+		select wp_posts.ID, wp_posts.post_date, wp_posts.post_content, wp_posts.post_title, wp_posts.guid, wp_terms.name, CONVERT(wp_postmeta.meta_value,UNSIGNED INTEGER) as count
+		from wp_posts
+		join wp_term_relationships on wp_posts.ID = wp_term_relationships.object_id
+		join wp_term_taxonomy on wp_term_relationships.term_taxonomy_id= wp_term_taxonomy.term_taxonomy_id
+		join wp_terms on wp_term_taxonomy.term_id=wp_terms.term_id
+		join wp_postmeta on wp_posts.ID=wp_postmeta.post_id
+		where wp_posts.post_status='publish' and wp_postmeta.meta_key='wpb_post_views_count' and wp_posts.post_type='post' and WEEKOFYEAR(NOW())=WEEKOFYEAR(wp_posts.post_date)
+		GROUP BY wp_posts.ID
+		order by meta_value desc
+		limit 10";
+
+		$post = $this -> db -> query($query);
+		return $post -> result_array();
+
+	}
+	
+	
+	function getimagebyidpost($id=0) {
+		$query = "
+			select guid from wp_posts
+	where wp_posts.post_parent=".$id." and wp_posts.post_mime_type='image/jpeg'";
+
+		$post = $this -> db -> query($query);
+		$rowcount = $post->num_rows();
+		$result= $post -> result_array();
+		if($rowcount>0)
+		{
+			return $result[0]['guid'];
+		}else return '';
+		
+	}
+	
+	
 
 }
 ?>
